7768f4b81e7a0788d4a50215939a4253f6284176
Gabriel Reis <gabrielreis.santos@outlook.com>
2025-07-06 13:34:57 -0300

    adicionando botão e sua funcionalidade

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-/* arquivo que vai conter o código JavaScripts */
+const botao = document.getElementById('mensagemBtn');
+const mensagemTexto = document.getElementById('mensagemTexto');
+
+botao.addEventListener('click', () => {
+    mensagemTexto.style.display = 'block';
+    mensagemTexto.textContent = 'Por mais difíceis que pareçam os caminhos, nunca desista dos seus sonhos eles são a bússola que guia seu coração para o que realmente importa. 😉';
+});
